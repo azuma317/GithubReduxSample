7d3be807176f61d1dd264a27c3b7b948520d7520
Azuma Sato <lifeghtcloerce5291@gmail.com>
2018-08-10 00:13:33 +0900

    add action

---
+++ GithubReduxSample/Actions/MainStateAction.swift
@@ -0,0 +1,19 @@
+//
+//  MainStateAction.swift
+//  GithubReduxSample
+//
+//  Created by Azuma on 2018/08/10.
+//  Copyright © 2018 Azuma. All rights reserved.
+//
+
+import Foundation
+import ReSwift
+
+enum MainStateAction: Action {
+    case fetchNextRepositories(totalPages: Int, repositories: [Repository<Owner>])
+    case showRepositoryDetail(Repository<Owner>)
+    case hideRepositoryDetail
+    case readySearch
+    case search(String)
+    case cancelSearch
+}
